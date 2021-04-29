81df9b25e5d725c03ad3539a9ce56fe9df589999
jihuder <julioarenas85@gmail.com>
2021-04-29 13:33:57 -0500

    [fix ~ 6-print_comb3](error correction)

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,9 @@
 for i in range(10):
     for j in range(10):
         if (i != j) and (i < j):
-            print("{}".format(i), end="")
-            print("{}".format(j), end=" ,")
+            if (i == 8) and (j == 9):
+                print("{}".format(i), end="")
+                print("{}".format(j))
+            else:
+                print("{}".format(i), end="")
+                print("{}".format(j), end=", ")
