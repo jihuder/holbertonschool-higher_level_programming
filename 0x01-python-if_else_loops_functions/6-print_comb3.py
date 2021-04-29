29181d50076fe6423257e116c3790559bf00a91a
jihuder <julioarenas85@gmail.com>
2021-04-29 18:57:38 -0500

    [Style ~ 6-print_comb3](style correcion)

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,8 +3,7 @@ for i in range(10):
     for j in range(10):
         if (i != j) and (i < j):
             if (i == 8) and (j == 9):
-                print("{}".format(i), end="")
-                print("{}".format(j))
+                print("{}{}".format(i, j))
             else:
                 print("{}".format(i), end="")
                 print("{}".format(j), end=", ")
