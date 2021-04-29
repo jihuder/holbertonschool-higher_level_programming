e7e789c47b8d95f0d781abc129d91611ca9f1d9f
jihuder <julioarenas85@gmail.com>
2021-04-28 20:46:30 -0500

    [fix  ~ 1-last_digit](function correct)

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,8 @@ number = random.randint(-10000, 10000)
 
 ultimo = abs(number) % 10
 if ultimo > 5:
-    print("and is greater than 5")
+    print("greater than 5")
 elif ultimo == 0:
-    print("and is 0")
+    print("0")
 else:
-    print("and is less than 6 and not 0")
+    print("less than 6 and not 0")
