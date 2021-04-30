117c4c4a1ce3e03804342c923916e48fa41f6483
jihuder <julioarenas85@gmail.com>
2021-04-29 20:24:31 -0500

    [feat ~ 8-uppercase](new function)

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    copy = ""
+    for i in str:
+        if ord(i) in range(97, 123):
+            copy += chr(ord(i) - 32)
+        else:
+            copy += i
+    print(copy)
