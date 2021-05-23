10b8badc858bf5eea73b04261a96ba9e0a818c91
jiuder <julioarenas85@gmail.com>
2021-05-22 21:18:00 -0500

    [Style ~ 2-rectangle](Correction Style)

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -11,18 +11,26 @@ class Rectangle:
 
     @property
     def height(self):
+        """height getter"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """height setter """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
     @width.setter
     def width(self, value):
+        """width setter"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -30,9 +38,11 @@ class Rectangle:
         self.__width = value
 
     def area(self):
+        """area method"""
         return self.__width * self.__height
 
     def perimeter(self):
+        """perimeter method"""
         if self.height == 0 or self.width == 0:
             return 0
         return 2 * (self.__width + self.__height)
