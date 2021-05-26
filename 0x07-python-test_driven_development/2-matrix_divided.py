cbe26805e495bd3f16b24274553e1270a2a06c36
jiuder <julioarenas85@gmail.com>
2021-05-25 21:26:09 -0500

    [Feat ~ 2-matrix_divided](New Function)

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that divides two integers"""
+
+
+def matrix_divided(matrix, div):
+    """function that divides two number"""
+    if type(matrix) in not [int, float]:
+        raise matrix must be a matrix +
+                         (list of lists) of integers/floats
+
+matrix = [[1, 2, 3],[4, 5, 6]]
