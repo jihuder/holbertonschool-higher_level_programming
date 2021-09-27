f8384c6431e506d55188f93f16976958ba0aa747
jihuder <julioarenas85@gmail.com>
2021-09-27 16:54:59 -0500

    [Feat ~ 2-safe](Prints List Items)

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    contador = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+        except (ValueError, TypeError):
+            continue
+        else:
+            contador += 1
+        print("")
+        return contador
