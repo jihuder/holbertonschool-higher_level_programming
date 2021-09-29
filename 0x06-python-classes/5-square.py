fe373a462382215a705b4d6351e0b71100c401ce
jihuder <julioarenas85@gmail.com>
2021-09-28 19:43:14 -0500

    [Style ~ 5-square](Style Correction)

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -19,14 +19,17 @@ class Square:
             if value < 0:
                 raise ValueError('size must be >= 0')
             self.__size = value
-            """area"""
-            def area(self):
-                return(self.__size ** 2)
-            """my_print"""
-            def my_print(self):
-                if self.size == 0:
-                    print('')
-                for i in range(self.size):
-                    for j in range(self.size):
-                        print('#', end='')
-                    print('')
+
+        """area"""
+        def area(self):
+            return(self.__size ** 2)
+
+        """my_print"""
+        def my_print(self):
+            if self.size == 0:
+                print('')
+            for i in range(self.size):
+                for j in range(self.size):
+                    print('#', end='')
+                print('')
+            
