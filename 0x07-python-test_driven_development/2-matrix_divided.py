8552fae54faabb0db726c33d618df3bc313b59fb
jihuder <julioarenas85@gmail.com>
2021-10-06 16:48:41 -0500

    [Correction ~ 2-matrix_divided](Correction File)

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,11 +1,35 @@
 #!/usr/bin/python3
-"""function that divides two integers"""
+
+"""function that divides all elements of a matrix. """
 
 
 def matrix_divided(matrix, div):
-    """function that divides two number"""
-    if type(matrix) in not [int, float]:
-        raise matrix must be a matrix +
-                         (list of lists) of integers/floats
+    """
+    Divides the elements of the matrix by
+    means of div, in case of error it is
+    because the elements are not valid
+    otherwise it returns the matrix with
+    its divided elements.
+    """
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a matrix " +
+                        "(list of lists) of integers/floats")
+    x = 0
+    for j in matrix:
+        for i in j:
+            if type(i) not in [int, float]:
+                raise TypeError("matrix must be a matrix " +
+                                "(list of lists) of integers/floats")
 
-matrix = [[1, 2, 3],[4, 5, 6]]
+        if x == 0:
+            x = len(j)
+        elif x != len(j):
+            raise TypeError("Each row of the matrix must have the same size")
+
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return [[round(n / div, 2) for n in j] for j in matrix]
