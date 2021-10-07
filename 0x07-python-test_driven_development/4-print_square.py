29e65ae6297ada7549025bd88585d6b92ecae1f9
jihuder <julioarenas85@gmail.com>
2021-10-06 21:26:32 -0500

    [Feat ~ 4-print_square](Print Square)

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" that prints a square with the character #"""
+
+def print_square(size):
+     """ Frame size"""
+
+     if type(size) is not int:
+         raise TypeError("size must be an integer")
+     if size < 0:
+         raise ValueError("size must be >= 0")
+     for i in range(size):
+         for j in range(size):
+             print('#', end="")
+         print()
