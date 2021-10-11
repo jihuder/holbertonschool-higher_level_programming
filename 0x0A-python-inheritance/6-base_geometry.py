b5ae0d5fc07a836bf58f66c7a4ffc3140b02397a
jihuder <julioarenas85@gmail.com>
2021-10-11 17:56:34 -0500

    [Feat ~ 6-base_geometry](Define Area)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+We start defining
+    the class geometry and area
+"""
+
+
+class BaseGeometry:
+    """ Class Basegeometryy """
+
+    def area(self):
+        """ Method that raises an Exception """
+        raise Exception("area() is not implemented")
