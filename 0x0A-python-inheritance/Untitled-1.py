3c5bb2f5a772be412da4573a3e923a52a190ed61
jihuder <julioarenas85@gmail.com>
2021-10-11 13:01:47 -0500

    [Feat ~ Untitled-1](Return Methods)

---
+++ 0x0A-python-inheritance/Untitled-1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Returns the list of the different
+        methods that the object has. """
+
+
+def lookup(obj):
+    """ returns the list of available
+            attributes and methods of an object """
+    return dir(obj)
