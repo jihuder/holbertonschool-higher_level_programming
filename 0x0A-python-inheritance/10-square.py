12a43bafc9c843bda4510aebfc80db66395e52f6
jihuder <julioarenas85@gmail.com>
2021-10-11 22:26:33 -0500

    [Feat ~ 10-square](Square inherintance rectangle)

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+In this module we start
+    building the Square class from
+                inherintance Rectangle
+"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ Square inherits from Rectangle """
+
+    def __init__(self, size):
+        """ method init a Square """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ method returns the area"""
+        return super().area()
