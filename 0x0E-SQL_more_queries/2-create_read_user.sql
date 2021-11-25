ad04cea7a02ba3dc9bfffafd443ef3a4969a3b79
jihuder <julioarenas85@gmail.com>
2021-11-24 19:34:33 -0500

    [Feat ~ 2-create](Update)

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
 -- Creates a user and database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL ON *.* TO user_0d_2@localhost;
-GRANT SELECT ON *.* TO user_0d_2@localhost;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
