17469973b434f9ec2f927e4da7265999f1ff2815
jihuder <julioarenas85@gmail.com>
2021-12-13 19:48:01 -0500

    [Exc ~ 1-mult](Permits)

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,5 @@
 #!/usr/bin/node
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+const len_C = 'C is fun';
+const len_Py = 'Python is cool';
+const len_Ja = 'JavaScript is amazing';
+console.log(len_C + '\n' + len_Py + '\n' + len_Ja);
