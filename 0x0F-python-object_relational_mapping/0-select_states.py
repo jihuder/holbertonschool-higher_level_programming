e459ee2818a8f255bedb462b53c2d40aad7be817
jihuder <julioarenas85@gmail.com>
2021-12-19 19:58:28 -0500

    [Feat ~ 0-select](List States)

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 ''' list all states from the database hbtn_0e_0_usa'''
 
-
-from sys import argv
-import MySQLdb
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
-cr = db.cursor()
-cr.execute("SELECT * from states ORDER BY states.id")
-states = cr.fetchall()
-for state in states:
-    print(state)
-cr.close()
-db.close()
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cr = db.cursor()
+    cr.execute("SELECT * from states ORDER BY states.id")
+    states = cr.fetchall()
+    for state in states:
+        print(state)
+    cr.close()
+    db.close()
