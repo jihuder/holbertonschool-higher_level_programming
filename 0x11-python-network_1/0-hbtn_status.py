479b23791214fe345ce2f7316ff02eafce215a9b
jihuder <julioarenas85@gmail.com>
2022-01-09 23:48:51 -0500

    Status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,15 @@
 """script that fetches https://intranet.hbtn.io/status"""
 
 
-if __name__ == "__main__":
+import urllib.request
 
-    import urllib.request
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
+
+if __name__ == "__main__":
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
